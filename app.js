a136485683c4991ec4d26f46e2a77e4df73137ec
Umair Memon <159755216+UmairMemon3341@users.noreply.github.com>
2025-12-17 21:49:56 +0500

    task drop all column feature completed

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
-const todo = document.querySelector('#todo');
-const progress = document.querySelector('#progress');
-const done = document.querySelector('#done');
+const todo = document.querySelector("#todo");
+const progress = document.querySelector("#progress");
+const done = document.querySelector("#done");
+let dragElement = null;
 
-const tasks = document.querySelectorAll('.task')
+const tasks = document.querySelectorAll(".task");
 
-tasks.forEach(tasks=>{
-    tasks.addEventListener('drag',(e)=>{
-        console.log('dragging', e);
-        
-    })
-})
+tasks.forEach((tasks) => {
+  tasks.addEventListener("drag", (e) => {
+    dragElement = tasks;
+  });
+});
 
-function addDragEventsOnColumn(column){
-    column.addEventListener('dragenter',(e)=>{
-        e.preventDefault();
-        column.classList.add('hover-over');
-    })
-    column.addEventListener('dragleave',(e)=>{
-        e.preventDefault();
-        column.classList.remove('hover-over')
-    })
+function addDragEventsOnColumn(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  });
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  });
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    column.appendChild(dragElement);
+    column.classList.remove("hover-over");
+  });
 }
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
+
+const toggleModal = document.querySelector("#toggle-modal");
+const modal = document.querySelector(".modal");
+const modalBg = document.querySelector(".modal .bg");
+const addNewTask = document.querySelector("#add-new-task");
+
+toggleModal.addEventListener("click", () => {
+  modal.classList.toggle("active");
+});
+
+modalBg.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+addNewTask.addEventListener("click", () => {
+  const taskTitleInp = document.querySelector("#task-title-inp").value;
+  const taskDesInp = document.querySelector("#task-des-inp").value;
+  const div = document.createElement("div");
+  div.classList.add("task");
+  div.setAttribute("draggable", "true");
+
+  div.innerHTML = `
+    <h2>${taskTitleInp}</h2>
+    <p>${taskDesInp}</p>
+    <button>Delete</button>                
+  `;
+  todo.appendChild(div)
+  modal.classList.remove('active')
+
+  div.addEventListener('drag',()=>{
+    dragElement = div
+  })
+});
